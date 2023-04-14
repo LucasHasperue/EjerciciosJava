ede2b3c1e0da12a7bc66f1eb930f3bc3cd65d6df
lucashasperue <lucashasperue@gmail.com>
2023-04-14 20:27:04 -0300

    agregandoEjercicios

---
+++ src/Subprogramas/Ejercicio1.java
@@ -0,0 +1,16 @@
+/*
+Crea una aplicación que le pida dos números al usuario y este pueda elegir entre 
+sumar,restar, multiplicar y dividir. La aplicación debe tener una función para
+cada operación matemática y deben devolver sus resultados para imprimirlos en el main. 
+ */
+package Subprogramas;
+
+
+public class Ejercicio1 {
+
+  
+    public static void main(String[] args) {
+       
+    }
+    
+}
